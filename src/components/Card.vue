9546933c0689044a5ec6232975065fbac57f7070
Matthew Kirkley <matt.kirkley@gmail.com>
2021-07-28 22:13:37 -0400

    Card is coming along, looking nicer

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,25 +1,31 @@
 <template>
   <div>
-    <div class="card">
+    <!-- <div class="card">
       <div class="card-inner red">
         <span class="mark">
-          3
+          {{ cardNumber }}
         </span>
       </div>
-    </div>
+    </div> -->
 
     <div class="card">
       <div class="card-inner green">
+        <span class="accent accent-top">
+          {{ cardNumber }}
+        </span>
         <span class="mark">
-          3
+          {{ cardNumber }}
+        </span>
+        <span class="accent accent-bottom">
+          {{ cardNumber }}
         </span>
       </div>
     </div>
 
-    <div class="card">
+    <!-- <div class="card">
       <div class="card-inner yellow">
         <span class="mark">
-          3
+          {{ cardNumber }}
         </span>
       </div>
     </div>
@@ -27,20 +33,28 @@
     <div class="card">
       <div class="card-inner blue">
         <span class="mark">
-          3
+          {{ cardNumber }}
         </span>
       </div>
-    </div>
+    </div> -->
   </div>
 </template> 
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      cardNumber: 7, // TODO: This will be a prop
+    };
+  },
+};
 </script>
 
 <style scoped>
+@import url(https://fonts.googleapis.com/css?family=Source+Sans+Pro:900);
+
 .card .card-inner.blue {
-  background: #0063b3;
+  background-color: #0063b3;
 }
 
 .card .card-inner.yellow {
@@ -56,27 +70,58 @@ export default {};
 }
 
 .card {
+  font-family: 'Source Sans Pro', sans-serif;
+  margin: 2rem;
   display: flex;
   align-items: stretch;
   height: 10rem;
-  width: 6rem;
+  width: 7rem;
   border-radius: 0.5rem;
   box-shadow: 0 0 10px #aaaaaa;
+  color: white;
+  text-shadow: 1px 1px 0 #000000, -1px -1px 0 #000000, -1px 1px 0 #000000, 1px -1px 0 #000000, 1px 0 0 #000000, -1px 0 0 #000000, 0 -1px 0 #000000, 0 1px 0 #000000, 4px 4px 0 #000000;
 }
 
 .card .card-inner {
   display: flex;
+  position: relative;
   align-items: center;
   justify-content: center;
   flex: auto;
   border-radius: 0.5rem;
   margin: 0.25rem;
   overflow: hidden;
-  background-color: #006383;
+  background-color: #006787;
+}
+
+.card .card-inner .accent {
+  position: absolute;
+  font-size: 1.25rem;
+  text-shadow: 1px 1px 0 #000000, -1px -1px 0 #000000, -1px 1px 0 #000000, 1px -1px 0 #000000, 1px 0 0 #000000, -1px 0 0 #000000, 0 -1px 0 #000000, 0 1px 0 #000000, 2px 2px 0 #000000;
+}
+
+.card .card-inner .accent.accent-top {
+  top: 0;
+  left: 0.5rem;
+}
+
+.card .card-inner .accent.accent-bottom {
+  bottom: 0;
+  right: 0.5rem;
 }
 
 .card .card-inner .mark {
-  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: transparent;
+  border-radius: 120px 60px / 120px 60px;
+  border: .25rem solid white;
+  height: 75%;
+  width: 100%;
+  font-size: 4.5rem;
+  margin-left: -0.25rem;
+  margin-right: -0.25rem;
 }
 
 /* .card .card-inner .mark {
